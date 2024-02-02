bd7b638a1a9ad26d8ad9efec1e9e0b86ba51b39e
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-02-02 18:20:20 -0300

    feat: teste nodemailer vercel

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -4,7 +4,9 @@ require('dotenv').config()
 export const enviarNotificacaoEmail = async (mensagem: string) => {
   // Configurações do transporte de e-mail (usando Gmail como exemplo)
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
       user: 'ronanfs89@gmail.com',
       pass: process.env.PASSWORD_APP,
