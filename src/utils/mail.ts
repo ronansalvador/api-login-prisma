e9486d8196d121af809d1a0e8be64001e04f77b3
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-02-02 18:27:47 -0300

    feat: teste com variaveis de ambiente

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -4,8 +4,8 @@ require('dotenv').config()
 export const enviarNotificacaoEmail = async (mensagem: string) => {
   // Configurações do transporte de e-mail (usando Gmail como exemplo)
   const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 465,
+    host: process.env.SMTP,
+    port: process.env.SMTP_PORT,
     secure: true,
     auth: {
       user: 'ronanfs89@gmail.com',
