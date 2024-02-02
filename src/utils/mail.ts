b57405d26cad1ea57454937784d7b13633a450ce
Ronan Salvador <ronansalvador@yahoo.com.br>
2024-02-02 20:02:53 -0300

    feat: novo teste com tls

--- src/utils/mail.ts
+++ src/utils/mail.ts
@@ -3,6 +3,18 @@ require('dotenv').config()
 
 export const enviarNotificacaoEmail = async (mensagem: string) => {
   // Configurações do transporte de e-mail (usando Gmail como exemplo)
+
+  // let transporter = nodemailer.createTransport({
+  //   service: 'gmail',
+  //   auth: {
+  //     type: 'OAuth2',
+  //     user: process.env.MAIL_USERNAME,
+  //     pass: process.env.MAIL_PASSWORD,
+  //     clientId: process.env.OAUTH_CLIENTID,
+  //     clientSecret: process.env.OAUTH_CLIENT_SECRET,
+  //     refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+  //   },
+  // })
   // const transporter = nodemailer.createTransport({
   //   host: process.env.SMTP,
   //   port: process.env.SMTP_PORT,
@@ -13,18 +25,24 @@ export const enviarNotificacaoEmail = async (mensagem: string) => {
   //   },
   // })
 
+  const { SMTP, SMTP_PORT, MAIL_USERNAME, PASSWORD_APP } = process.env
+
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    host: SMTP,
+    port: SMTP_PORT,
+    secure: false,
     auth: {
-      user: 'frederique28@ethereal.email',
-      pass: '6EbWNpr8VCgQByRfw1',
+      user: MAIL_USERNAME,
+      pass: PASSWORD_APP,
+    },
+    tls: {
+      rejectUnauthorized: false,
     },
   })
 
   // Detalhes do e-mail
   const mailOptions = {
-    from: 'frederique28@ethereal.email',
+    from: 'ronanfs89@gmail.com',
     to: 'ronansalvador@yahoo.com.br',
     subject: 'TESTE GMAIL',
     text: mensagem,
